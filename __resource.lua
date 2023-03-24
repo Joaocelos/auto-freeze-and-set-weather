5c10efe4edd0a1353abd757acc41b5dae9abba6f
João Celos <76658833+Joaocelos@users.noreply.github.com>
2023-03-24 20:42:32 +0000

    Create __resource.lua

---
+++ __resource.lua
@@ -0,0 +1,13 @@
+-- Resource Metadata
+fx_version 'cerulean'
+games { 'gta5' }
+
+author 'Celos'
+description 'This script will set and freeze the weather in the chosen type.'
+version '1.0.0'
+
+-- What to run
+client_scripts {
+    'client.lua',
+    'config.json'
+}
